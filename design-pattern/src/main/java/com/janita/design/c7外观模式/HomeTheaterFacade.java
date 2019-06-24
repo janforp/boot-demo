00df1416b2542a344981b14c34169822b89ff825
zhucj <janforp@163.com>
2019-06-24 19:15:50 +0800

    外观模式

---
+++ design-pattern/src/main/java/com/janita/design/c7外观模式/HomeTheaterFacade.java
@@ -0,0 +1,13 @@
+package com.janita.design.c7外观模式;
+
+/**
+ * 类说明：外观模式
+ * <p>
+ * 封装一堆子系统，让客户看到简单清楚的外观，堆客户封装了变化
+ * </p>
+ *
+ * @author zhucj
+ * @since 2019-06-24 - 16:38
+ */
+public class HomeTheaterFacade {
+}
