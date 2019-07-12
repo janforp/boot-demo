2bc7d4f5f40f48161d6636f9c62f8248d98be30a
zhucj <janforp@163.com>
2019-07-12 15:45:55 +0800

    使用抽象工厂造鸭子

--- weekly-report/src/main/java/com/janita/weekly/controller/WeeklyReportController.java
+++ weekly-report/src/main/java/com/janita/weekly/controller/WeeklyReportController.java
@@ -13,14 +13,11 @@ import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.ParseException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.spring.web.json.Json;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
