a903e7c06c1b1a4b369e2253c179017ba896707b
zhucj <janforp@163.com>
2019-07-26 16:43:22 +0800

    多线程直接贡献spring上下文

--- thread-parent/requestAttributes-share-in-multi-thread/src/main/java/com/janita/share/requestattributes/sycn/ContextCopyingDecorator.java
+++ thread-parent/requestAttributes-share-in-multi-thread/src/main/java/com/janita/share/requestattributes/sycn/ContextCopyingDecorator.java
@@ -1,8 +1,11 @@
 package com.janita.share.requestattributes.sycn;
 
+import org.slf4j.MDC;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.Map;
 
 // https://stackoverflow.com/questions/23732089/how-to-enable-request-scope-in-async-task-executor
 public class ContextCopyingDecorator implements TaskDecorator {
@@ -12,8 +15,10 @@ public class ContextCopyingDecorator implements TaskDecorator {
         return () -> {
             try {
                 RequestContextHolder.setRequestAttributes(context);
+                Map<String, String> copyOfContextMap = MDC.getCopyOfContextMap();
                 runnable.run();
             } finally {
+                MDC.clear();
                 RequestContextHolder.resetRequestAttributes();
             }
         };
