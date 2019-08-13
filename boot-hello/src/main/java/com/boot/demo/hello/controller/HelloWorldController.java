09fdb0640129d51daa50713132e247ca2db20cb3
zhucj <janforp@163.com>
2019-08-13 09:32:11 +0800

    异常捕获相关

--- boot-hello/src/main/java/com/boot/demo/hello/controller/HelloWorldController.java
+++ boot-hello/src/main/java/com/boot/demo/hello/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.boot.demo.hello.controller;
 
+import com.boot.demo.hello.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class HelloWorldController {
 
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping("/hello")
     public String hello() {
 
         return "hello word";
+    }
+
+    @GetMapping("throwA")
+    public String throwEx() {
+        helloService.throwExcep();
+        return "a";
+    }
+
+    @GetMapping("throwB")
+    public String throwB() {
+       try {
+           helloService.throwExcep();
+           return "a";
+       }catch (Exception e) {
+           System.out.println(e);
+       }
+       return "";
     }
 }
